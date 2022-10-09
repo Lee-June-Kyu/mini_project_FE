f72a76e31bd4ce06e2b12fc15f2691a47318eb7a
mickeyhunu <110079472+mickeyhunu@users.noreply.github.com>
2022-10-09 13:54:05 +0900

    Add files via upload

---
+++ src/views/AttendanceHistoryView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>출결관리화면</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/NotepadView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>알림장</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
