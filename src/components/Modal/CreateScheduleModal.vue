e21e524607568e2d0469fb0f44f85bc61fd163f9
Lee-June-Kyu <leejuk95@naver.com>
2022-10-18 10:07:02 +0900

    스케쥴 추가 UI 작성

---
+++ src/components/Modal/CreateScheduleModal.vue
@@ -0,0 +1,126 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="1000">
+    <v-card class="modal">
+      <div class="modalHeader">
+        <h2>스케줄 추가</h2>
+        <v-btn icon text @click="closeModal">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </div>
+
+      <form>
+        <h3>날짜 선택</h3>
+        <v-menu
+          v-model="menu"
+          :close-on-content-click="false"
+          :nudge-right="40"
+          transition="scale-transition"
+          offset-y
+          min-width="auto"
+        >
+          <template #activator="{ on, attrs }">
+            <v-text-field
+              v-model="date"
+              label="Day"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              dense
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="date" @input="menu = false"></v-date-picker>
+        </v-menu>
+        <v-simple-table fixed-header height="300px">
+          <template #default>
+            <thead>
+              <tr>
+                <th class="text-left">강의 시간</th>
+                <th class="text-left">학생 목록</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, i) in items" :key="item.name">
+                <td>{{ item }}</td>
+                <td>
+                  <v-container fluid>
+                    <v-select v-model="studentsValue[i]" :items="students" label="학생 선택" multiple>
+                      <template #selection="{ item, index }">
+                        <v-chip v-if="index === 0">
+                          <span>{{ item }}</span>
+                        </v-chip>
+                        <span v-if="index === 1" class="grey--text text-caption">
+                          (+{{ studentsValue[i].length - 1 }} others)
+                        </span>
+                      </template>
+                    </v-select>
+                  </v-container>
+                </td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+        <div class="modalBottom">
+          <div></div>
+          <v-btn @click="createSchedule">추가하기</v-btn>
+        </div>
+      </form>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  props: {
+    openDialog: Boolean
+  },
+  data() {
+    return {
+      date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000).toISOString().substr(0, 10),
+      menu: false,
+      items: ['1시', '2시', '3시', '4시', '5시', '6시', '7시', '8시', '9시', '10시'],
+      students: ['이준규', '우나은', '이상훈', '남경화', '원영준', '박태환'],
+      studentsValue: [[]]
+    }
+  },
+  computed: {
+    dialog(props) {
+      return props.openDialog
+    }
+  },
+  methods: {
+    closeModal() {
+      this.$emit('closeDialog')
+    },
+    async createSchedule() {
+      console.log(this.date)
+      console.log(this.studentsValue)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  box-sizing: border-box;
+}
+
+.modalHeader {
+  display: flex;
+  margin: 0px 40px 40px 40px;
+  padding-top: 40px;
+  justify-content: space-between;
+}
+
+.textField {
+  margin: auto;
+  width: 90%;
+}
+
+.modalBottom {
+  display: flex;
+  justify-content: space-between;
+  padding: 40px;
+}
+</style>
